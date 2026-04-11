dfec260b9569f1791402d64bef6882682378ab4a
Gxy0921 <1935872921@qq.com>
2026-04-11 16:36:58 +0800

    newloop

--- reaction.py
+++ reaction.py
@@ -3,18 +3,45 @@ from time import sleep
 from random import uniform
 
 led=LED(4)
-right_button=Button(15)
-left_button=Button(14)
+right_button=Button(15,bounce_time=0.1)
+left_button=Button(14,bounce_time=0.1)
 
-led.on()
-sleep(uniform(5,10))
-led.off()
+left_name=input("Left plater name is:")
+right_name=input("Right plater name is:")
+
+round_num=1
+
+round_active=True
 
 def pressed(button):
-    print(str(button.pin.number)+'won the game')
+    global round_active
+    if round_active:
+       round_active=False
+       if button.pin.number==14:
+          print(left_name+'won the game')
+       else:
+          print(right_name+'won the game')
 
-right_button.when_pressed=pressed
-left_button.when_pressed=pressed
+try:
+    while True:
+       round_active=True
 
-while True:
-    sleep(0.1)
+       print(f"\n the{round_num}round")
+       sleep(1)
+
+       led.on()
+       sleep(uniform(5,10))
+       led.off()
+
+       right_button.when_pressed=pressed
+       left_button.when_pressed=pressed
+
+       while round_active:
+           sleep(0.05)
+
+       round_num+=1
+       sleep(1)
+
+except KeyboardInterrupt:
+    print('Game over')
+    led.close()
